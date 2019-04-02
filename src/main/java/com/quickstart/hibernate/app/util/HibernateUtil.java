05a1c14f23dcef35de0a8346151a6094b2a9377d
Gabriel Casarin da Silva <gabriel.casarin1@ibm.com>
2019-04-02 13:34:53 -0300

    Initial commit

---
+++ src/main/java/com/quickstart/hibernate/app/util/HibernateUtil.java
@@ -0,0 +1,44 @@
+package com.quickstart.hibernate.app.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Create registry
+            registry = new StandardServiceRegistryBuilder().configure().build();
+
+            // Create MetadataSources
+            MetadataSources sources = new MetadataSources(registry);
+
+            // Create Metadata
+            Metadata metadata = sources.getMetadataBuilder().build();
+
+            // Create SessionFactory
+            sessionFactory = metadata.buildSessionFactory();
+        } catch (Exception ex) {
+            System.out.println(ex);
+            if (registry != null) {
+                StandardServiceRegistryBuilder.destroy(registry);
+            }
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
